ae5eae1dd45ba393982615a31bb0758400aabbb5
tiinapajukari <tiina.pajukari@aalto.fi>
2017-03-05 17:47:30 +0200

    mllmllm

--- slideshow.js
+++ slideshow.js
@@ -1,6 +1,35 @@
+var index = 1
 
 window.onload = function () {
+  'use strict';
     $.getJSON("https://pajukat1-40fdc.firebaseio.com/.json", function (data) {
-        console.log(data);
+        //console.log(data);
+        $('#otsikko').hmtl(data).uutiset[index].otsikko)});
+}
+
+function display() {
+    'use strict';
+    $.getJSON("https://pajukat1-40fdc.firebaseio.com/.json", function (data) {
+        $('#otsikko').html(data.uutiset[index].otsikko);
+        $('#paivamaara').html(data.uutiset[index].paivamaara);
+        $('#sisalto').html(data.uutiset[index].sisalto);
     });
+}
+
+function switchA() {
+    'use strict';
+    setInterval(function () {
+        nextSlide();
+    }, 3000);
+}
+
+function nextSlide() {
+    'use strict';
+    if (index < 2) {
+        index += 1;
+        display();
+    } else {
+        index = 0;
+        display();
+    }
 }
